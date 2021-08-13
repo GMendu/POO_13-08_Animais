b79e967de3130879e7deb9565a67281e47550872
Gabriel Mendonça <bielm2000@hotmail.com>
2021-08-13 15:43:16 -0300

    salvando animais 1.0

--- atividade1/Form2.cs
+++ atividade1/Form2.cs
@@ -10,17 +10,23 @@ namespace atividade1
 {
     public partial class Form2 : Form
     {
+        int x;
         public Form2(int x)
         {
             InitializeComponent();
+            this.x = x;
             txtNumAnimal.Text = x.ToString();
-            string[] Animais = new string[4,x];
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             Animal A1 = new Animal();
+            string[,] animal = new string[4, x];
+            for(int j = x; j > 0; j--)
+            for (int i = 0; i < 4; i++)
+            {
+                animal[i, x] = txtTipo.Text;
+            }
 
         }
     }
